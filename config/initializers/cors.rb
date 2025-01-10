670454dd72c6fbbc06722da1168decbd67073ee7
picardsflutex <picardsflutex1@gmail.com>
2025-01-10 19:02:36 +0200

    FIX: cors configure

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000'
+    origins 'http://localhost:3000', 'https://store-frontend-5afz.vercel.app'
     resource(
       '*',
       headers: :any,
